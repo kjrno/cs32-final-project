16ba814712cc2e165da3d79c8e5d9f8b9aea577f
kjrno <kylerrno@college.harvard.edu>
2026-04-10 03:02:35 +0000

    Thu, Apr 9, 2026, 11:02 PM -04:00

--- symptomchecker.py
+++ symptomchecker.py
@@ -2,14 +2,14 @@
 diseases = {
     "Flu": {"fever", "cough", "body aches", "fatigue", "chills"},
     "Cold": {"runny nose", "cough", "sore throat"}
-    "
 
-}
+
+} #Sa
 
 user_symptoms = {"fever", "cough"}
 
 def score_conditions(user_symptoms, diseases):
-    scores = {}
+    scores = {} #empty dictionary holds results
     for condition_name, condition_symptoms in diseases.items():
         matched = condition_symptoms & user_symptoms
         score = len(matched) / len(condition_symptoms)
